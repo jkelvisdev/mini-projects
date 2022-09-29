d88dfcc8bbf684e2681e6c4b6e8c1b63887d363e
iskelvs <jkelvisdev23@gmail.com>
2022-09-29 08:42:21 -0500

    Updating input validator and some prints. JK

--- python_course/mastermind.py
+++ python_course/mastermind.py
@@ -29,71 +29,78 @@ ia = numbers[:4]
 stars = 0
 ats = 0
 attemps = 0
+flag = True
 
 #starting the game
 
-while True:
+while flag:
+	attemps += 1
+	print()
 	print(f"Your ats are: {ats}")
 	print(f"Your stars are: {stars}")
-	print(f"Attemps left: {attemps}")
-
-	attemps += 1
-	if attemps > 15:
-		print()
-		print(f" ### Game over, you've exceded the 15 attemps ### ")
-		break
-
-	user_number = input('Insert a 4 digits number -> ')
+	print(f"Attemps left: {15-attemps}")
 	print()
 
-	if len(user_number) != 4:
+#Validator
+	while True:
+		user_number = input('Insert a 4 digits number -> ')
 		print()
-		print("---- Number must be 4 digits -----")
-		break
-
-	repeated = []
-#checking for repeated numbers
-	for n in range(4):
-		if int(user_number[n]) not in repeated:
-			repeated.append(int(user_number[n]))
+		repeated = []
+		if len(user_number) == 4:
+			for n in range(4):
+				if int(user_number[n]) not in repeated:
+					repeated.append(int(user_number[n]))
+				else:
+					print()
+					print(" !!! Number must be different !!!")
+					print()
 		else:
 			print()
-			print("---- Numbers must be different ----")
+			print(' ### Numbers must be 4 digits long ### ')
+			print()
+		if len(repeated) == 4:
 			break
 
+#defining rewards or winner
 	if repeated == ia:
 		print()
-		print(f"You won, number guessed '{ia}'")
+		print(f" ######### You won, number guessed '{ia} ######### '")
 		break
 
 	for x in range(4):
-		counter = 0
 		if repeated[x] in ia:
 			if ia[x] == repeated[x]:
 				stars += 1
-				print(f"You won 1 star")
+				print(f" ### You won 1 star ### ")
 			else: 
 				ats += 1
-				print(f"You won 1 at")
+				print(f" ### You won 1 at ###")
+
 	print()
-	print(f"You have {ats:4} ats")
+	print(f"You have {ats} ats")
 	print()
-	print(f"You have {stars:4} stars")
+	print(f"You have {stars} stars")
 	print()
-	print(f"Attemps have {attemps:4} attemps left")
+	print(f"Attemps have {attemps} attemps left")
 	print()
-	print(repeated, user_number, ia)
+
+	if attemps > 15:
+		print()
+		print(f" ### Game over, you've exceded the 15 attemps ### ")
+		flag = False
+
 	while True:
-		user_option = input("Do you want to keep playing? - (y/n)").lower()
-		
+		user_option = input("Do you want to keep playing?(y/n) -> ").lower()
+			
 		if user_option == 'y':
 			break
 		elif user_option == 'n':
+			flag = False
 			print()
-			print('Game closed')
+			print('### Game was closed. ###')
+			print()
 			break
-		break
 		else:
 			print()
-			print('I did not understand your answer')
+			print("I didn't understand your answer")
 			print()
