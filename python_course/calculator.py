e092cab988381f05a5c6aa8b54b8dda740da2428
iskelvs <jkelvisdev23@gmail.com>
2022-10-07 08:43:33 -0500

    Finishing the documents and working in validator logic. jk

--- python_course/calculator.py
+++ python_course/calculator.py
@@ -15,7 +15,7 @@ def welcome():
 	print()
 
 #menu options screen
-def menu()
+def menu():
 	print()
 	print(" ----> Type the number of the option that you want <----")
 	print()
@@ -24,6 +24,9 @@ def menu()
 	print("3. For MULTIPLICATION press 3 -> ")
 	print("4. For DIVISION press 4 -> ")
 	print()
+
+def user_input():
+	input("Type an option -> ")
 
 def multiply(number1,number2):
 #function that return multiplication result between 2 numbers 
@@ -40,37 +43,57 @@ def sum(number1, number2):
 def substract(number1, number2):
 	return number1 - number2
 
-def options(user_option):
-#function to validate user option.
-	if user_option == '1':
-		return sum(n1,n2)
-	elif user_option == '2':
-		return substract(n1,n2)
-	elif user_option == '3':
-		return multiply(n1,n2)
-	elif user_option == '4':
-		return div(n1,n2)
+def validator(number1, number2):
+	if n1 == int and n2 == int:
+		return True
 	else:
 		return None
-
 
 flag = True
 
 while flag:
-	#welcome screens
+	#welcome screen
 	welcome()
 	menu()
 
 	#saving the user input
 	user_option = user_input()
 
-	#chosing the request.
-	if options(user_option) != None:
-		n1 = int(input("Type first number"))
-		n2 = int(input("Type second number"))
-		result = options(user_option)
-		print(f"The result is {result}")
-		print()
-		break
+	#input validator.
+	while True:
+
+		n1 = int(input("Type a number -> "))
+		n2 = int(input("Type a second number -> "))
+		print(validator(n1,n2))
+
+		if validator is True:
+			break
+		else:
+			print("You must type a valid option.")
+			print()
+
+	#doing the operations.
+	if user_option == '1':
+		print(f"The result is: {sum(n1, n2)} ")
+	elif user_option == '2':
+		print(f"The result is: {substract(n1, n2)} ")
+	elif user_option == '3':
+		print(f"The result is: {multiply(n1, n2)} ")
+	elif user_option == '4':
+		print(f"The result is: {div(n1, n2)} ")
 	else:
-		()
+		print("You must chose a valid option to operate")
+
+	#Checking if the user wants to keep the calculator open
+	while True:
+		option = input("Do you want to continue -> (y/n)")
+
+		if option.lower() == 'n':
+			print(" ---- Calculator was closed ---- ")
+			break
+			flag = False
+		elif option.lower() == 'y':
+			break
+		else:
+			print("You must type a valid option.")
+			print()
